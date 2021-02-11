59b288e1154034e704e885dd1fd667261a8c1c36
MomenSherif <momensherif.2019@gmail.com>
2021-02-11 20:40:21 +0200

    📦 NEW: ListingDetailsScreen & ListItem Component

--- App.js
+++ App.js
@@ -2,17 +2,12 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Card from './app/components/Card';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: '#f8f4f4', padding: 20, paddingTop: 100 }}>
-      <Card
-        title="Red jacket for sale!"
-        subtitle="$100"
-        image={require('./app/assets/jacket.jpg')}
-      />
-    </View>
+    <ListingDetailsScreen />
   );
 }
 
---
+++ app/screens/ListingDetailsScreen.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+
+import AppText from '../components/AppText';
+import ListItem from '../components/ListItem';
+import colors from '../config/colors';
+
+export default function ListingDetailsScreen() {
+  return (
+    <View>
+      <Image style={styles.image} source={require('../assets/jacket.jpg')} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Red jacket for sale!</AppText>
+        <AppText style={styles.price}>$100</AppText>
+
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require('../assets/demon-slayer.jpg')}
+            title="Demon Slayer"
+            subtitle="5 Listings"
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  detailsContainer: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '500'
+  },
+  price: {
+    color: colors.secondary,
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 10
+  },
+  userContainer: {
+    marginVertical: 40,
+  },
+});
