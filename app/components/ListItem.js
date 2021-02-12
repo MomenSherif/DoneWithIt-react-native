0c99fc2ffcc62dd2a9d79bd1d37a69f816b2844f
MomenSherif <momensherif.2019@gmail.com>
2021-02-12 15:52:46 +0200

    Flastlist Separator

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -19,6 +19,7 @@ export default function ListItem({ title, subtitle, image }) {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
+    padding: 15,
   },
   image: {
     width: 70,
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 
 import ListItem from '../components/ListItem';
+import ListItemSeparator from '../components/ListItemSeparator';
 import Screen from '../components/Screen';
 
 const messages = [
@@ -25,6 +26,7 @@ export default function MessagesScreen() {
       <FlatList
         data={messages}
         keyExtractor={(message) => message.id.toString()}
+        ItemSeparatorComponent={ListItemSeparator}
         renderItem={({ item }) => (
           <ListItem
             title={item.title}
