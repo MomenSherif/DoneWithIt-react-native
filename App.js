3afb72e3e4f66b0b1a1f7c82751e4a600fa96409
Momen Sherif <momensherif.2019@gmail.com>
2021-07-18 11:19:45 +0200

    🚧 PROGRESS: Stack Navigator

--- App.js
+++ App.js
@@ -1,7 +1,76 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { Button, Text } from 'react-native';
+import {
+  NavigationContainer,
+  useNavigation,
+  useRoute,
+} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import Screen from './app/components/Screen';
+
+const Tweets = () => {
+  const navigation = useNavigation();
+
+  return (
+    <Screen>
+      <Text>Tweets</Text>
+      <Button
+        title="Details"
+        onPress={() =>
+          navigation.navigate('TweetDetails', {
+            id: 10,
+            name: "Mo'men",
+            age: 25,
+          })
+        }
+      />
+    </Screen>
+  );
+};
+
+const TweetDetails = () => {
+  const {
+    params: { id, name, age },
+  } = useRoute();
+
+  return (
+    <Screen>
+      <Text>Tweet Details - {id}</Text>
+      <Text style={{ fontSize: 20 }}>
+        {name} {age}
+      </Text>
+    </Screen>
+  );
+};
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: 'crimson',
+      },
+      headerTintColor: '#fff',
+    }}
+  >
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen
+      name="TweetDetails"
+      component={TweetDetails}
+      options={({ route }) => ({
+        title: `Tweet ${route.params.name}`,
+      })}
+    />
+  </Stack.Navigator>
+);
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <>
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+    </>
+  );
 }
